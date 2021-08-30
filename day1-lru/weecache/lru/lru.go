8e66b73e3a7bb09ead4e305dae5a935a2fdad9f1
sungn2 <sungn2@lenovo.com>
2021-08-30 23:10:53 +0800

    Add get and remove func.

--- day1-lru/weecache/lru/lru.go
+++ day1-lru/weecache/lru/lru.go
@@ -9,14 +9,45 @@ type Cache struct {
 	maxBytes  int64
 	nBytes    int64
 	cache     map[string]*list.Element
-	OnEvicted func(key string, value Value)
+	OnEvicted func(key string, value Mvalue)
 }
 
-type Value interface {
+type Mvalue interface {
 	Len() int
 }
 
 type entry struct {
 	key   string
-	value Value
+	value Mvalue
+}
+
+func New(maxBytes int64, onEvicted func(string, Mvalue)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+func (c *Cache) GET(key string) (value Mvalue, ok bool) {
+	if elm, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(elm)
+		kv := elm.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
+
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nBytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
 }
