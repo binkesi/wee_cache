af94a01e70db4f940166f99f6d7d04947d48bcfe
binkesi <sjtusgn@163.com>
2021-09-03 14:36:00 +0800

    Add api server and cache server start.

--- day5-multi-nodes/main.go
+++ day5-multi-nodes/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"multi-nodes/weecache"
+	"net/http"
 )
 
 var db = map[string]string{
@@ -24,5 +25,30 @@ func createGroup() *weecache.Group {
 }
 
 func startCacheServer(addr string, addrs []string, wee *weecache.Group) {
+	peers := weecache.NewHTTPPool(addr)
+	peers.Set(addrs...)
+	wee.RegisterPeers(peers)
+	log.Println("weecache is running at", addr)
+	log.Fatal(http.ListenAndServe(addr[7:], peers))
+}
 
+func startAPIServer(apiAddr string, wee *weecache.Group) {
+	http.Handle("/api", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			key := r.URL.Query().Get("key")
+			view, err := wee.Get(key)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/octet-stream")
+			w.Write(view.ByteSlice())
+		}))
+	log.Println("fontend server running at", apiAddr)
+	log.Fatal(http.ListenAndServe(apiAddr[7:], nil))
+}
+
+func main() {
+	var api bool
+	var port int
 }
