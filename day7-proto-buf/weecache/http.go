2502d8a68d9a638a186fe0d744b52b624816d670
binkesi <sjtusgn@163.com>
2021-09-06 10:44:13 +0800

    use protobuf

--- day7-proto-buf/weecache/http.go
+++ day7-proto-buf/weecache/http.go
@@ -15,33 +15,73 @@ import (
 )
 
 const (
-	defaultPath     = "/_weecache/"
+	defaultBasePath = "/_weecache/"
 	defaultReplicas = 50
 )
 
+// HTTPPool implements PeerPicker for a pool of HTTP peers.
 type HTTPPool struct {
+	// this peer's base URL, e.g. "https://example.net:8000"
 	self        string
 	basePath    string
-	mu          sync.Mutex
+	mu          sync.Mutex // guards peers and httpGetters
 	peers       *consistenthash.Map
-	httpGetters map[string]*httpGetter
+	httpGetters map[string]*httpGetter // keyed by e.g. "http://10.0.0.2:8008"
 }
 
-type httpGetter struct {
-	baseURL string
-}
-
+// NewHTTPPool initializes an HTTP pool of peers.
 func NewHTTPPool(self string) *HTTPPool {
 	return &HTTPPool{
 		self:     self,
-		basePath: defaultPath,
+		basePath: defaultBasePath,
 	}
 }
 
+// Log info with server name
 func (p *HTTPPool) Log(format string, v ...interface{}) {
 	log.Printf("[Server %s] %s", p.self, fmt.Sprintf(format, v...))
 }
 
+// ServeHTTP handle all http requests
+func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
+		panic("HTTPPool serving unexpected path: " + r.URL.Path)
+	}
+	p.Log("%s %s", r.Method, r.URL.Path)
+	// /<basepath>/<groupname>/<key> required
+	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
+	if len(parts) != 2 {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	groupName := parts[0]
+	key := parts[1]
+
+	group := GetGroup(groupName)
+	if group == nil {
+		http.Error(w, "no such group: "+groupName, http.StatusNotFound)
+		return
+	}
+
+	view, err := group.Get(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Write the value to the response body as a proto message.
+	body, err := proto.Marshal(&pb.Response{Value: view.ByteSlice()})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(body)
+}
+
+// Set updates the pool's list of peers.
 func (p *HTTPPool) Set(peers ...string) {
 	p.mu.Lock()
 	defer p.mu.Unlock()
@@ -53,6 +93,7 @@ func (p *HTTPPool) Set(peers ...string) {
 	}
 }
 
+// PickPeer picks a peer according to key
 func (p *HTTPPool) PickPeer(key string) (PeerGetter, bool) {
 	p.mu.Lock()
 	defer p.mu.Unlock()
@@ -65,32 +106,8 @@ func (p *HTTPPool) PickPeer(key string) (PeerGetter, bool) {
 
 var _ PeerPicker = (*HTTPPool)(nil)
 
-func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if !strings.HasPrefix(r.URL.Path, p.basePath) {
-		panic("HTTPPool serving unexepted path: " + r.URL.Path)
-	}
-	p.Log("%s %s", r.Method, r.URL.Path)
-	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
-	if len(parts) != 2 {
-		http.Error(w, "bad request", http.StatusBadRequest)
-		return
-	}
-	groupName := parts[0]
-	key := parts[1]
-
-	group := GetGroup(groupName)
-	if group == nil {
-		http.Error(w, "no such group"+groupName, http.StatusNotFound)
-		return
-	}
-
-	body, err := proto.Marshal(&pb.Response{Value: view.ByteSlice()})
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/octet-stream")
-	w.Write(body)
+type httpGetter struct {
+	baseURL string
 }
 
 func (h *httpGetter) Get(in *pb.Request, out *pb.Response) error {
@@ -98,18 +115,23 @@ func (h *httpGetter) Get(in *pb.Request, out *pb.Response) error {
 		"%v%v/%v",
 		h.baseURL,
 		url.QueryEscape(in.GetGroup()),
-		url.QueryEscape(in.Getkey()),
+		url.QueryEscape(in.GetKey()),
 	)
 	res, err := http.Get(u)
 	if err != nil {
 		return err
 	}
 	defer res.Body.Close()
+
 	if res.StatusCode != http.StatusOK {
-		return fmt.Errorf("server returned %s", res.Status)
+		return fmt.Errorf("server returned: %v", res.Status)
 	}
 
 	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return fmt.Errorf("reading response body: %v", err)
+	}
+
 	if err = proto.Unmarshal(bytes, out); err != nil {
 		return fmt.Errorf("decoding response body: %v", err)
 	}
